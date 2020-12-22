47b3f8e4dbe0a90215f900475b1547a5cb3145f6
morimori8282 <moriwaki8282@yahoo.co.jp>
2020-12-22 10:35:44 +0900

    first commit

---
+++ database/migrations/2020_12_18_051309_create_reservations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReservationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id');
+            $table->dateTime('start', 0);
+            $table->dateTime('end', 0);
+            $table->timestamps();
+            $table->date('deleted_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservations');
+    }
+}
---
+++ database/migrations/2020_12_21_143022_change_null_to_users_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeNullToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+            $table->string('officail_name', 255)->nullable()->change();
+            $table->string('phonetic_name', 255)->nullable()->change();
+            $table->string('salt', 255)->nullable()->change();
+            $table->integer('gender')->nullable()->change();
+            $table->date('birthday')->nullable()->change();
+            $table->string('tel', 255)->nullable()->change();
+            $table->string('zip', 255)->nullable()->change();
+            $table->integer('pref')->nullable()->change();
+            $table->string('address', 255)->nullable()->change();
+            $table->string('building', 255)->nullable()->change();
+            $table->string('remark', 255)->nullable()->change();
+            $table->date('deleted_at')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}
